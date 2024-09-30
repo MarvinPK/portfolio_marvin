2ab039f9162ba192ea6819e9f33184125c222274
Prioux-khaye Marvin <marvin.priouxkhaye@hotmail.fr>
2024-09-30 10:38:34 +0200

    feat v1 bis home

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
           <p><h1>Hi, I am <b>Marvin</b></h1></p>
         </div>
         <div className='bouncy-title-two'>
-          <p><h1> a <b>fullstack</b> and <b>smiling</b> developpeur.</h1></p>
+          <p><h1> a <b>fullstack</b> and <b>smiling</b> 😊 developpeur.</h1></p>
         </div>
       </div>  
       <div id="picture">
