00ce815e3f0bef1556e68fe316d197a36030c44c
Prioux-khaye Marvin <marvin.priouxkhaye@hotmail.fr>
2024-09-30 10:21:06 +0200

    feat: v1 Home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
 import Technologies from './components/technologies/Technologies';
@@ -6,10 +6,35 @@ import Projects from './components/project/Projects';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import Experiences from './components/experiences/Experiences';
+import "./App.css"
 
 function App() {
+  const followerRef = useRef(null); // Créer une référence pour le follower
+
+  useEffect(() => {
+      const follower = followerRef.current;
+
+      // Fonction pour déplacer l'élément follower
+      const handleMouseMove = (e) => {
+          const x = e.clientX; // Position X de la souris
+          const y = e.clientY + window.scrollY; // Position Y de la souris avec le défilement
+          
+          // Déplacer l'élément en suivant le curseur
+          follower.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+      };
+
+      // Ajouter l'événement de mouvement de la souris
+      document.addEventListener('mousemove', handleMouseMove);
+
+      // Nettoyer l'événement au démontage du composant
+      return () => {
+          document.removeEventListener('mousemove', handleMouseMove);
+      };
+  }, []); // [] signifie que l'effet ne s'exécute qu'une fois au montage
+
   return (
     <div>
+     <div class="follower" ref={followerRef}></div>
       <Navbar />
       <Hero />
       <Technologies/>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,17 +4,14 @@ import "./hero.css"
 const Hero = () => {
   return (
     <section id="hero" className="hero-section">
-      <div id="presentation">
-       <div className='bouncy-title'>
+      <div className='title'>
+       <div className='bouncy-title-one'>
           <p><h1>Hi, I am <b>Marvin</b></h1></p>
-          <p><h1> a <b>fullstack</b> and <b>smiling</b> developpeur.</h1></p>
-       </div> 
-       <div className='bouncy-triangle'>       
-        <div class="triangle-border">
-          <div class="triangle-inner"></div>
         </div>
-      </div>
-      </div>
+        <div className='bouncy-title-two'>
+          <p><h1> a <b>fullstack</b> and <b>smiling</b> developpeur.</h1></p>
+        </div>
+      </div>  
       <div id="picture">
         <img src="assets/img/me.jpg"/>
       </div>
